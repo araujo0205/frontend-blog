56b492152da81fffe0d7e61064d07b8909254cab
David Souza <david@ditcom.com.br>
2017-11-23 17:59:52 -0200

    Adicionado helper.js e criado o Home.spec.js

--- test/Categories.spec.js
+++ test/Categories.spec.js
@@ -1,15 +1,23 @@
 import { mount} from 'vue-test-utils';
 import Categories from "../src/Categories.vue";
 import expect from 'expect';
+import Helpers from './Helper.js';
+
+require('./setup.js');
 
 describe('Categories', () => {
-    let wrapper = mount(Categories);
+    let wrapper, h;
 
+    beforeEach (() => {
+        wrapper = mount(Categories);
+        h = new Helpers(wrapper, expect);
+    });
+    
     it ('total size of categories variable', () => {
         expect(wrapper.vm.cats.length).toBe(2);
     });
     
     it ('presents the categorie desenvolvimento', () => {
-        expect(wrapper.html()).toContain("desenvolvimento");
-    });
+        h.see('desenvolvimento');
+    }); 
 })
--- test/Home.spec.js
+++ test/Home.spec.js
@@ -1,15 +1,24 @@
 import { mount} from 'vue-test-utils';
-import Categories from "../Home.vue";
+import Home from "../src/Home.vue";
 import expect from 'expect';
 
-describe('Categories', () => {
-    let wrapper = mount(Home);
+describe('Home', () => {
+    let wrapper;
+    beforeEach (() => {
+        wrapper = mount(Home);
+    });
 
-    it ('total size of categories variable', () => {
-        expect(wrapper.vm.cats.length).toBe(2);
+    it ('total articles loaded', () => {
+        expect(wrapper.vm.articles.length).toBe(2);
     });
+
+    it ('list first item', () => {
+        see('Testando algoritmos');
+    });
+
+    let see = (text, selector) => {
+        let wrap = selector ? wrapper.find(selector) : wrapper;
     
-    it ('presents the categorie desenvolvimento', () => {
-        expect(wrapper.html()).toContain("desenvolvimento");
-    });
+        expect(wrap.html()).toContain(text);
+    };
 })
